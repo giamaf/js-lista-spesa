f281104040c4ce77928bab97474d8afc81495e8e
giamaf <gianluca.maffucci@hotmail.com>
2024-01-08 17:16:58 +0100

    js_added 'while' cicle + DOM's print

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('JS OK');
 
+//todo --------------- GENERICS --------------- \\  
+
 // Recupero gli elementi dal DOM
 const listBox = document.getElementById('list-box');
 
@@ -10,14 +12,33 @@ console.table(buyList);
 // Inizializzo la variabile "stringa" che stamperò in pagina
 let listItems = '';
 
-// Creo un ciclo per stampare la lista della spesa in pagina
-for (let i = 0; i < buyList.length; i++) {
+//todo ---------------------------------------- \\  
+
+//* Creo un ciclo 'for' per stampare la lista della spesa in pagina
+// for (let i = 0; i < buyList.length; i++) {
+//     listItems +=
+//         `<div class="form-check m-2">
+//              <input class="form-check-input" type="checkbox">
+//              <label class="form-check-label">${buyList[i]}</label>
+//         </div>`;
+// }
+
+// Stampo in pagina agganciando la section del DOM
+// listBox.innerHTML = listItems;
+
+
+//! Creo un ciclo 'while' per stampare la lista della spesa in pagina
+// Inizializzo la variabile "contatore"
+let i = 0;
+
+while (i < buyList.length) {
     listItems +=
         `<div class="form-check m-2">
-        <input class="form-check-input" type="checkbox">
-        <label class="form-check-label">${buyList[i]}</label>
-    </div>`;
-
+            <input class="form-check-input" type="checkbox">
+            <label class="form-check-label">${buyList[i]}</label>
+        </div>`;
+    i++;
 }
-console.log(listItems);
+
+// Stampo in pagina agganciando la section del DOM
 listBox.innerHTML = listItems;
