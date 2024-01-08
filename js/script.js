2de50fec5490a73f5bce1c126940699899c1c542
giamaf <gianluca.maffucci@hotmail.com>
2024-01-08 16:59:31 +0100

    js prep to cicle

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('JS OK');
+
+// Recupero gli elementi dal DOM
+const listBox = document.getElementById('list-box');
+
+// Creo la lista della spesa
+const buyList = ['latte', 'biscotti', 'acqua', 'pasta', 'frutta', 'verdura'];
+console.table(buyList);
+
+// Inizializzo la variabile "stringa" che stamperò in pagina
+let listItems = '';
